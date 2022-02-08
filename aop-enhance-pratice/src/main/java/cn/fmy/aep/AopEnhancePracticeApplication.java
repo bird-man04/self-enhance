abb0cb10496acd965e0e94e24405e2e8fd4286a8
fmy <fengmingyang04@aliyun.com>
2022-02-08 13:58:11 +0800

    New practice project.

---
+++ aop-enhance-pratice/src/main/java/cn/fmy/aep/AopEnhancePracticeApplication.java
@@ -0,0 +1,13 @@
+package cn.fmy.aep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AopEnhancePracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AopEnhancePracticeApplication.class, args);
+    }
+
+}
---
+++ aop-enhance-pratice/src/test/java/cn/fmy/aep/AopEnhancePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.fmy.aep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AopEnhancePracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
